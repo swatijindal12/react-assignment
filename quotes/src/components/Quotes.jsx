6f924b36168eddcfa3270555451689ae6532421b
Swati Jindal <swatij@mindfiresolutions.com>
2023-06-29 19:37:17 +0530

    fixed minor issues

--- quotes/src/components/Quotes.jsx
+++ quotes/src/components/Quotes.jsx
@@ -6,43 +6,51 @@ import LoaderContainer from './LoaderContainer'
 import axios from 'axios'
 
 const Quotes = () => {
-  const [quoteText, setQuoteText] = useState('')
-  const [quoteAuthor, setQuoteAuthor] = useState('')
   const [isLoading, setIsLoading] = useState(true)
+  const [quotesData, setQuotesData] = useState([])
+  const [quote, setQuote] = useState({ text: '', author: '' })
 
   useEffect(() => {
-    const fetchQuote = async () => {
+    const fetchQuotes = async () => {
       try {
         const res = await axios.get('https://type.fit/api/quotes')
         const quotesData = res.data
-        const randomValue = Math.floor(Math.random() * quotesData.length) // generate random value
-        setQuoteText(quotesData[randomValue].text)
-        setQuoteAuthor(quotesData[randomValue].author)
-        setIsLoading(false)
+        setQuotesData(quotesData)
       } catch (error) {
-        console.error('Error fetching quote:', error)
+        console.error('Error fetching quotes:', error)
       }
     }
 
-    const interval = setInterval(fetchQuote, 8000)
+    fetchQuotes()
+  }, [])
+
+  useEffect(() => {
+    setIsLoading(true)
+    const interval = setInterval(() => {
+      const randomValue = Math.floor(Math.random() * quotesData.length)
+      const { text, author } = quotesData[randomValue]
+      setQuote({ text, author })
+      setIsLoading(false)
+      console.log('Quote text is:', quote)
+    }, 8000)
 
     return () => {
       clearInterval(interval)
     }
-  }, [])
+  }, [quotesData])
 
   return (
     <div className="main">
       {isLoading ? (
         <>
-          <img src={loader} alt="LoaderImg" />
+          <div className="spin-loader"></div>
         </>
       ) : (
         <div className="quotes">
           {' '}
           <img src={doubleQuotes} alt="Double-quotes" />
-          <h2>{quoteText}</h2>
-          <p className="author">{quoteAuthor}</p>
+          <h2>{quote.text}</h2>
+          <p className="author">{quote.author}</p>
           <LoaderContainer />
         </div>
       )}
